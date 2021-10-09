1752845bec34592c316a59e87b01a045882824c8
Charlie <charliepbarth@gmail.com>
2021-10-09 17:41:52 -0400

    First draft of my TicTacToe app

---
+++ main.py
@@ -0,0 +1,11 @@
+class TicTacToe:
+    win_results = []
+    
+    def __init__(self):
+        self.board = [None] * 9
+
+    def game_start():
+        return 0
+
+    def  game_over():
+        return 0
